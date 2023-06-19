941386136f351bbdf0534478c8ca33564b340b17
CrisTinaFLN <crisjf.tina@hotmail.com>
2023-06-18 23:19:52 -0300

    Correção do  cadastro de Nova Unidade

--- src/pages/Unidades/Unidades.jsx
+++ src/pages/Unidades/Unidades.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-// import UnitList from '../../components/organismos/UnitList/UnitList';
-// import UnitRegister from '../../components/organismos/UnitRegister/UnitRegister';
 import { Container, UnitList, UnitRegister } from '../../components';
 
 export default function Unidades(){
   const [openForm, setOpenForm] = useState(false);
-  const [selectedUnit, setSelectedUnit] = useState({});
+  const [selectedUnit, setSelectedUnit] = useState(undefined);
     return (
        <Container title="Unidades">
         {openForm === false && (
